5afd1943d4df381ce1371fdd119217cdeafa6386
Mobinhazar <mobinhazarkhani@gmail.com>
2025-06-17 19:02:37 +0330

    first try for navigation in first floor

---
+++ Assets/Scripts/SetTarget.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Camera topDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+
+    private NavMeshPath path;
+    private LineRenderer line;
+
+    void Start()
+    {
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+        line.positionCount = path.corners.Length;
+        line.SetPositions(path.corners);
+        line.enabled = true;
+    }
+}
